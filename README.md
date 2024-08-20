5520ec5b1260b4f58b97059547d174f0c3bb2f1c
Yohei Kakiuchi <kakiuchi.yohei.sw@tut.jp>
2024-08-20 19:11:37 +0900

    update simple_move

--- README.md
+++ README.md
@@ -15,6 +15,30 @@ rosrun rviz rviz -d simple_move.rviz ##
 - いい感じのところに 2D pose estimate (rviz上)
 - 適度なところへ goal を設定 (rviz上)
 
+### dummy odom
+
+twistを積算するダミーのオドメトリを使用する
+
+```
+roslaunch run_gazebo.launch use_dummy_odom:=true ## gazeboのlaunch, ダミーのオドメトリ
+./dummy_odom.py
+```
+
+### Making map
+
+マップを作る
+
+```
+roslaunch run_gazebo.launch making_map:=true ## マップを作る
+```
+
+cmd_velを出力するもの(twist_joyやtwist_keyboard)でロボットを動かす。
+
+マップの保存
+```
+rosrun map_server map_saver -f map
+```
+
 ## インストールが必要なもの
 
 他も必要かもしれない
@@ -27,7 +51,9 @@ ros-noetic-amcl \
 ros-noetic-map-server \
 ros-noetic-teb-local-planner \
 ros-noetic-dwa-local-planner \
-ros-noetic-global-planner
+ros-noetic-global-planner  \
+ros-noetic-slam-gmapping  \
+ros-noetic-teleop-twist-keyboard
 
 ```
 
@@ -38,3 +64,4 @@ URDF ファイルは修正の必要ないとは思うが
 xacro base.urdf.xacro > simple_move.urdf ## aero_ros_pkg 直下で
 
 libgazebo_force_move の所を修正
+
---
+++ dummy_odom.py
@@ -0,0 +1,98 @@
+import rospy
+import tf
+import math
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Twist
+
+_g_br_ = tf.TransformBroadcaster()
+_g_pub_ = rospy.Publisher('odom', Odometry)
+
+_g_x = 0.0
+_g_y = 0.0
+_g_th = 0.0
+
+_prev_vx = 0.0
+_prev_vy = 0.0
+_prev_vth = 0.0
+
+_prev_tm = None
+
+def publishOdom(tm, child_frame='base_link'):
+    om_msg = Odometry()
+
+    ## om_msg.header = twist_msg.header ##
+    om_msg.header.stamp = tm
+    om_msg.header.frame_id = 'odom'
+    om_msg.child_frame_id  = child_frame
+
+    om_msg.pose.covariance[0]  = 0.01
+    om_msg.pose.covariance[7]  = 0.01
+    om_msg.pose.covariance[14] = 10.0 #
+    om_msg.pose.covariance[21] = 10.0 #
+    om_msg.pose.covariance[28] = 10.0 #
+    om_msg.pose.covariance[35] = 0.1
+    om_msg.pose.pose.position.x = _g_x
+    om_msg.pose.pose.position.y = _g_y
+    om_msg.pose.pose.position.z = 0.0
+    om_msg.pose.pose.orientation.x = 0.0
+    om_msg.pose.pose.orientation.y = 0.0
+    om_msg.pose.pose.orientation.z = math.sin(_g_th*0.5)
+    om_msg.pose.pose.orientation.w = math.cos(_g_th*0.5)
+
+    om_msg.twist.covariance[0]  = 0.004
+    om_msg.twist.covariance[7]  = 0.004
+    om_msg.twist.covariance[14] = 10.0 #
+    om_msg.twist.covariance[21] = 10.0 #
+    om_msg.twist.covariance[28] = 10.0 #
+    om_msg.twist.covariance[35] = 0.04
+    om_msg.twist.twist.linear.x = _prev_vx # twist_msg.twist.linear.x
+    om_msg.twist.twist.linear.y = _prev_vy # twist_msg.twist.linear.y
+    ## om_msg.twist.twist.linear.z = 0.0
+    ##om_msg.twist.twist.angular.x = 0.0
+    ##om_msg.twist.twist.angular.y = 0.0
+    om_msg.twist.twist.angular.z = _prev_vth # twist_msg.twist.angular.z
+
+    _g_pub_.publish(om_msg)
+
+def publishTf(tm, child_frame='base_link'):
+    _g_br_.sendTransform((_g_x, _g_y, 0.0),
+                         (0.0, 0.0, math.sin(_g_th*0.5), math.cos(_g_th*0.5)),
+                         tm,      ## msg.header.stamp
+                         child_frame,
+                         'odom')
+
+def callback(*args):
+    now = rospy.Time.now()
+    publishOdom(now)
+    publishTf(now)
+
+def callback_twist(msg):
+    global _g_x, _g_y, _g_th, _prev_vx, _prev_vy, _prev_vth, _prev_tm
+    now = rospy.Time.now()
+    if _prev_tm is None:
+        _prev_vx  = msg.linear.x
+        _prev_vy  = msg.linear.y
+        _prev_vth = msg.angular.z
+        _prev_tm  = now
+        return
+    else:
+        tm = (now - _prev_tm).to_sec()
+        #
+        cs = math.cos(_g_th)
+        ss = math.sin(_g_th)
+        _g_x  += tm * ( cs * msg.linear.x - ss * msg.linear.y )
+        _g_y  += tm * ( ss * msg.linear.x + cs * msg.linear.y )
+        _g_th += tm * msg.angular.z
+
+        _prev_vx  = msg.linear.x
+        _prev_vy  = msg.linear.y
+        _prev_vth = msg.angular.z
+        _prev_tm  = now
+
+if __name__ == '__main__':
+    rospy.init_node('dummy_odom_publisher')
+
+    sub = rospy.Subscriber('cmd_vel', Twist, callback_twist)
+    tm  = rospy.Timer(rospy.Duration(0.04), callback)
+
+    rospy.spin()
